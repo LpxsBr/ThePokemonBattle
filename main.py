341753cf64b27d4330fc71660ff070d40f31776a
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-06-28 16:35:19 +0000

    life system model

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print('welcome to pokemon battle')
+x = 10
+print('vida = ', x)
+
+# how we going to coding the 'life system'
+
+while x > 0:
+  f = int(input('digite aqui seu numero de atks '))
+  x = x - f
+  if(x == 0):
+    print('voce morreu, vida =', x)
+  else:
+    print('agora a vida é ',x)
