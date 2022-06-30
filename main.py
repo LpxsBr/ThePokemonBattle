f87061f5bf86858e020ef03a8ec76b3ecf51644d
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-06-30 12:11:14 +0000

    update the conditional logic -> print game over when 'x' is <= 0

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ print('vida = ', x)
 # how we going to code the 'life system'
 
 while x > 0:
+  print('chegou a hora de escolher seu pokemon!')
+  
+  # here, will be the pokemon choice
+  
   f = int(input('digite aqui seu numero de atks '))
   x = x - f
-  if(x == 0):
-    print('voce morreu, vida =', x)
+  if (x <= 0):
+      print('game over')
   else:
-    print('agora a vida é ',x)
+      print('agora a vida é ', x)
