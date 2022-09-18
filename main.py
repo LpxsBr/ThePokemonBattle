9527440d9f78b42aa538ba03ccb0acc0d542c73d
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-09-18 01:10:02 +0000

    add function to clear the terminal

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import os
+
 print('welcome to pokemon battle')
 x = 10
 print('vida = ', x)
 
 # how we going to code the 'life system'
+clear = lambda: os.system('clear')
+
+if(1):
+  print('chegou a hora de escolher seu pokemon!')
 
 while x > 0:
-  print('chegou a hora de escolher seu pokemon!')
   
   # here, will be the pokemon choice
   
   f = int(input('digite aqui seu numero de atks '))
   x = x - f
+  clear()
   if (x <= 0):
       print('game over')
   else:
